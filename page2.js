5c01de5fe4da02e0e24f4386384daa9955692337
hakkı bulur <bulut103@gmail.com>
2023-08-14 13:31:19 +0300

    GuncelCzm

--- page2.js
+++ page2.js
@@ -4,7 +4,7 @@
 //4  Elma dizinin bir elemanımıdır?
 //5  Kiraz meyvesini listenin sonuna ekleyiniz.
 //6  Dizinin son 2 elemanını siliniz.
-//7  Aşağıdaki bilgileri dizi içerisinde saklayınız ve her öğrencinin yaşını hesaplayınız.
+//7  Aşağıdaki bilgileri dizi içerisinde saklayınız ve her öğrencinin yaşını ve not ortalamasını hesaplayınız.
 /*
     öğrenci 1: Yiğit Bilgi 2010 (70,60,80)
     öğrenci 2: Ada Bilgi 2012   (80,80,90)
@@ -20,7 +20,11 @@ dizi.splice(-2)
 document.write("<br>güncel dizinin son 2 elemanı : "+dizi[dizi.length -1] + ", " + dizi[dizi.length -2])
 dizi.push(["yiğit", "Bilgi", 2010, [70,60,80]]);
 dizi.push(["Ada", "Bilgi", 2012, [80,80,90]]);
-dizi.push(["Ahmet", "Turan", 2009, [60,60,70]])
-document.write(`<br>${dizi[3][0]} adlı öğrencinin yaşı : ${2023 - dizi[3][2]}`);
-document.write(`<br>${dizi[4][0]} adlı öğrencinin yaşı : ${2023 - dizi[4][2]}`);
-document.write(`<br>${dizi[5][0]} adlı öğrencinin yaşı : ${2023 - dizi[5][2]}`);
+dizi.push(["Ahmet", "Turan", 2009, [60,60,70]])/*
+function notortalamasi(one,two,three) 
+    {
+        return ((one + two + three) / 3);
+    }*/
+document.write(`<br>${dizi[3][0]} adlı öğrencinin yaşı : ${new Date().getFullYear() - dizi[3][2]}  ve not ortalaması : ${((dizi[3][3][0] + dizi[3][3][1] + dizi[3][3][2]) / 3).toFixed(1)}`);
+document.write(`<br>${dizi[4][0]} adlı öğrencinin yaşı : ${new Date().getFullYear() - dizi[4][2]}  ve not ortalaması : ${((dizi[4][3][0] + dizi[4][3][1] + dizi[4][3][2]) / 3).toFixed(1)}`);
+document.write(`<br>${dizi[5][0]} adlı öğrencinin yaşı : ${new Date().getFullYear() - dizi[5][2]}  ve not ortalaması : ${((dizi[5][3][0] + dizi[5][3][1] + dizi[5][3][2]) / 3).toFixed(1)}`);
