feeb1ea57172b994d6fd730b8fa999efcb4d829f
hakkı bulur <bulut103@gmail.com>
2023-08-15 12:52:38 +0300

    Ugulama_Donguler_CevaplarYazildi

--- page2.js
+++ page2.js
@@ -29,26 +29,80 @@ document.write(`<br>${dizi[3][0]} adlı öğrencinin yaşı : ${new Date().getFu
 document.write(`<br>${dizi[4][0]} adlı öğrencinin yaşı : ${new Date().getFullYear() - dizi[4][2]}  ve not ortalaması : ${((dizi[4][3][0] + dizi[4][3][1] + dizi[4][3][2]) / 3).toFixed(1)}`);
 document.write(`<br>${dizi[5][0]} adlı öğrencinin yaşı : ${new Date().getFullYear() - dizi[5][2]}  ve not ortalaması : ${((dizi[5][3][0] + dizi[5][3][1] + dizi[5][3][2]) / 3).toFixed(1)}`);
 
-// yeni proje : 
+// yeni proje : --------------------------------------------------------------------------------------------------------------------------------------------
+document.write(" <br> yeni proje --------------------------------------------------");
 let sayilar = [1,5,7,15,3,25];
 
 //1- sayilar listesindeki her bir elemanın karesini yazdırınız.
+let kareliSayilar = " ";
+for (let i of sayilar) 
+{
+    kareliSayilar += (i*i) + " ";
+}
+document.write(`<br><br> sayılar listesindeki her bir eleman ${sayilar}  ve sırasıyla karesi : ${ kareliSayilar} `)
 
 //2- sayılar listesindeki hangi sayılar 5'in katıdır?
+    let besinKati = [];
+    let k = 0;
+    for(let i = 0; i <sayilar.length; i++)
+    {
+        if(sayilar[i] % 5 == 0)
+        {
+            besinKati[k] = sayilar[i];
+            k++;   
+        }
+    }
+    document.write(" <br> 5 in katı olanlar : "+ besinKati);
 
 //3- sayılar listesindeki çift sayıların toplamını bulunuz.
-
+let ciftToplam = 0;
+sayilar.forEach((sayi, indexi) => {
+    if(sayi % 2 == 0)
+        ciftToplam += sayi;
+});
+document.write("<br> dizideki çift sayıların toplamı : " + ciftToplam);
+//---------------------------
 let urunler = ["iphone 12", "samsung s22", "iphone 13", "samsung s23"];
 
 //4- urunler listesindeki tüm ürünleri büyük harf ile yazdırınız.
+document.write(`<br> elemanlar : ${urunler}  büyük harfle yazılmış halleri : `);
+for(let i of  urunler)
+{
+    document.write(i.toUpperCase() + ", ");
+}
 
 //5- urunler listesinde içinde samsung geçen kaç ürün vardır?
-
+let count = 0;
+for (let i of urunler)
+{
+    if(i.includes("samsung") == true)
+        count++;
+}
+document.write(" <br>listede içinde samsung geçen ürün sayısı : "+ count);
+//--------------------------------------------------------------------------------
 let ogrenciler = [
     {"Ad":"yiğit",  "Soyad":"bilgi",  "Notlar":[60,70,60]},
     {"Ad":"ada",  "Soyad":"bilgi",  "Notlar":[80,70,80]},
     {"Ad":"çınar",  "Soyad":"turan",  "Notlar":[70,70,60]},
 ];
 // ogrenciler listesindeki her öğrencinin not ortalaması ve başarı durumlarını yazdırınız.
-
+let notOrt_basariDrm = []
+for(let i = 0; i< ogrenciler.length; i++)
+{
+    notOrt_basariDrm[i] = { "nameAndSurname" : (ogrenciler[i].Ad + " " + ogrenciler[i].Soyad), "gradeAverage":((ogrenciler[i].Notlar[0] + ogrenciler[i].Notlar[1] + ogrenciler[i].Notlar[2]) / 3).toFixed(1)};
+}
+for(let i in notOrt_basariDrm)
+{
+    let basari = "başarısız...";
+    if(notOrt_basariDrm[i].gradeAverage>= 50)
+        basari = "başarılı";
+    document.write("<br>öğrenci ad-soyad : "+ notOrt_basariDrm[i].nameAndSurname + " ||||| Öğrenci not ortalaması : " + notOrt_basariDrm[i].gradeAverage + " başarı durumu : " + basari);
+}
 // tüm öğrencilerin not ortalaması kaçtır?
+let genelOrt = 0;
+for(let i in notOrt_basariDrm)
+{
+    genelOrt+= Number(notOrt_basariDrm[i].gradeAverage);
+}
+genelOrt = (genelOrt / 3).toFixed(1);
+document.write("<br> tüm öğrencilerin not ortalaması : " +genelOrt);
